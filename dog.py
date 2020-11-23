dd9a5235ed1bd3a3163f9077a72a9d1d60964eb6
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-11-23 10:43:11 -0400

    feat(new class): add dog class module
    This commit adds the module for the dog class with methods to bark, sit, roll. Also it adds a file called my_dogs.py where instances of the dog class are created and methods are used.

---
+++ dog.py
@@ -0,0 +1,14 @@
+class Dog:
+    def __init__(self, name, breed):
+        self.name = name
+        self.breed = breed
+        print('dog initialized!')
+
+    def bark(self):
+        print('Woof!')
+
+    def sit(self):
+        print(f'{self.name} sits')
+    
+    def roll(self):
+        print(f'{self.name} rolls over')
---
+++ my_dogs.py
@@ -0,0 +1,15 @@
+from dog import Dog
+
+my_dog = Dog('Rex', 'SuperDog')
+my_dog.bark()
+
+my_other_dog = Dog('Annie', 'SuperDog')
+print(my_other_dog.name)
+
+amigo = Dog('Amigo', 'SuperSuperDog')
+solo = Dog('Solo', 'SuperDuperDog')
+mando = Dog('Mando', 'SuperSuperDuperDog')
+
+amigo.sit()
+solo.roll()
+mando.bark()
