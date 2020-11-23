dbd5eb4fb5228b9dc695252ec8f35f69cf283a29
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2020-11-23 12:55:54 -0400

    feat(new class): add hero class
    This commit add the hero class in a new module and within two methods. The constructor method and the fight method.
    In the constructor method it adds the name, starting health (default to 100) and the current health.
    Inside the fight method the istructions are to choose one winner randomnly, for now.

---
+++ hero.py
@@ -0,0 +1,33 @@
+from random import choice
+
+class Hero:    
+    # We want our hero to have a default "starting_health", so we can set that in the function header.
+    def __init__(self, name, starting_health=100):
+        '''Instance properties:
+          name: String
+          starting_health: Integer
+          current_health: Integer
+        '''
+
+        # we know the name of our hero, so we assign it here
+        self.name = name
+        # similarly, our starting health is passed in, just like name
+        self.starting_health = starting_health
+        # when a hero is created, their current health is always the same as their starting health (no damage taken yet!)
+        self.current_health = starting_health
+
+    def fight(self, opponent):
+        '''Current Hero will take turns fighting the opponent hero passed in.'''
+
+        fighters = [self.name, opponent.name]
+        print(f'{choice(fighters)} won!')
+
+
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal this block is executed.
+    hero1 = Hero('Wonder Woman')
+    hero2 = Hero('Dumbledore')
+
+    hero1.fight(hero2)
